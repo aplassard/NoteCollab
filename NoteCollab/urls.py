5bd0e874c0535a7841ac68a6d7b7cca89b6808d1
aplassard <andrew.plassard@gmail.com>
2012-11-13 18:33:14 -0500

    added student url

--- NoteCollab/urls.py
+++ NoteCollab/urls.py
@@ -8,8 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'NoteCollab.views.home', name='home'),
     # url(r'^NoteCollab/', include('NoteCollab.foo.urls')),
-
+	url(r'^student/', student.urls.students),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
--- student/views.py
+++ student/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from student.models import student
+
+def students(request):
+	students = student.objects.all()
+	t=loader.get_template('student/index.html')
+	HttpResponse(t.render(Context({'s':students})))
