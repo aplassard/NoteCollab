964282767d5eb634256f3ec167e656e3affcd7b4
aplassard <andrew.plassard@gmail.com>
2012-11-13 13:57:34 -0500

    Added Student Model

--- student/models.py
+++ student/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class student(models.Model):
+	firstname = models.CharField(max_length=25)
+	lastname = models.CharField(max_length=25)
+	
+	GRADES=(
+    ('FR', 'Freshman'),
+    ('SO', 'Sophomore'),
+    ('JR', 'Junior'),
+    ('SR', 'Senior'),
+    ('GR', 'Graduate'),
+)
+	grade = models.CharField(max_length=10,choices=GRADES)
