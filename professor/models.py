31edca1f7be099bec0d8f7bc6557c1b58deac823
aplassard <andrew.plassard@gmail.com>
2012-11-13 14:05:32 -0500

    Added Professor and Department Model

--- professor/models.py
+++ professor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class professor(models.Model):
+	firstname = models.CharField(max_length=20)
+	lastname = models.CharField(max_lengh=20)
+	department = models.ForeignKey(department,null=True)
+
+class department(models.Model):
+	name = models.CharField(max_length=20)
