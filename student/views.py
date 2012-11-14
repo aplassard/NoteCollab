d42c9b332481d5f3e6271dc71dca735a13942948
aplassard <andrew.plassard@gmail.com>
2012-11-13 19:02:56 -0500

    updated template location

--- student/views.py
+++ student/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 def students(request):
 	students = student.objects.all()
 	t=loader.get_template('student/index.html')
-	HttpResponse(t.render(Context({'s':students})))
+	return HttpResponse(t.render(Context({'s':students})))
