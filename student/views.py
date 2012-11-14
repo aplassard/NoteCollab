4790bed31392bcf7358abc2f1ce6db79675b3de6
aplassard <andrew.plassard@gmail.com>
2012-11-14 14:41:16 -0500

    added student view

--- student/views.py
+++ student/views.py
@@ -6,3 +6,10 @@ def students(request):
 	students = student.objects.all()
 	t=loader.get_template('student/index.html')
 	return HttpResponse(t.render(Context({'s':students})))
+
+def info(request, pk):
+	s = student.objects.get(id=pk)
+	C = Context({'student':s})
+	t=loader.get_template('student/student.html')
+	return HttpResponse(t.render(C))
+
