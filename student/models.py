daf39b9cff94449cceaa768283781fd104b18565
aplassard <andrew.plassard@gmail.com>
2012-11-15 14:45:39 -0500

    Added Studnent Submithr

--- student/models.py
+++ student/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class student(models.Model):
-	firstname = models.CharField(max_length=25,null=True)
-	lastname = models.CharField(max_length=25,null=True)
+	firstname = models.CharField(max_length=25,null=True,blank=True)
+	lastname = models.CharField(max_length=25,null=True,blank=True)
 	
 	GRADES=(
     ('FR', 'Freshman'),
@@ -11,7 +11,7 @@ class student(models.Model):
     ('SR', 'Senior'),
     ('GR', 'Graduate'),
 )
-	grade = models.CharField(max_length=10,choices=GRADES,null=True)
+	grade = models.CharField(max_length=10,choices=GRADES,null=True,blank=True)
 
 	def __unicode__(self):
 		return self.firstname+' '+self.lastname
