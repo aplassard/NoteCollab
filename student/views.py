2403615ea2b364872e347ccc2a084beab2a126d4
aplassard <andrew.plassard@gmail.com>
2012-11-15 14:38:42 -0500

    Added Studnent Submit

--- student/views.py
+++ student/views.py
@@ -37,3 +37,8 @@ def submit(request,pk):
 
 def submitted(request,pk):
 	return render_to_response('student/submitted.html',{},context_instance=RequestContext(request))
+
+def new(request):
+	s=student()
+	s.save()
+	return redirect("/student/%s/" % s.id)
