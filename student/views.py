29defb748515d06613a08fc6948b6c66e298e0d4
aplassard <andrew.plassard@gmail.com>
2012-11-17 17:14:30 -0500

    Updated Student Form

--- student/views.py
+++ student/views.py
@@ -46,7 +46,7 @@ def delete(request):
 	o=""
 	for key in request.POST.keys():
 		o+=key+'\t'+request.GET[key]+"<br>"
-	return render_to_response('student/deleted.html',c,context_instance=RequestContext(request))
+	return render_to_response('student/deleted.html',RequestContext(request, {}))
 #	a=get_object_or_404(student, id=pk)
 #	n=a.firstname+' '+a.lastname
 #	a.delete()
