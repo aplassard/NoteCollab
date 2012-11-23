161e8a43e9e42fc162db7e871b0a593cadab388f
aplassard <andrew.plassard@gmail.com>
2012-11-23 11:23:46 -0500

    Testing socket heroku database

--- NoteCollab/settings.py
+++ NoteCollab/settings.py
@@ -13,8 +13,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'notecollab.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -159,6 +159,8 @@ LOGGING = {
     }
 }
 
-import dj_database_url
-DATABASES['default']=dj_database_url.config()
+import socket
+if socket.gethostname().find("-")>0:
+	import dj_database_url
+	DATABASES['default']=dj_database_url.config()
 
